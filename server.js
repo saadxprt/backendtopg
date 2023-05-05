84ac1facd0d537930dda02c8d7949337f5b188fc
saadxprt <96834166+saadxprt@users.noreply.github.com>
2023-05-05 22:30:53 +0500

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const http = require("http");
 const myVariable = "4d0d9d9d4b7cab8986aa90db0c7ed07964a8acf5405bbf746aebd7f1c80e87b6";
 
 const server = http.createServer((req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.write(myVariable);
     res.end();
 });
 
-server.listen(80, () => {
+server.listen(8080, () => {
     console.log("Server running on http://localhost:8080/");
 });
